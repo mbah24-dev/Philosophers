fe7cf5a5c4d89e9e7378badbc4f4bc332fbb07a8
BAH Mamad <mbah@student.42lyon.fr>
2025-04-07 17:49:53 +0200

    ✨ Refactorisation et amélioration de la gestion des arguments 🔧 Mise à jour du fichier main.c pour intégrer une fonction de vérification des arguments plus robuste et plus lisible. 🔑 Ajout de la vérification des arguments avec des messages d’erreur plus clairs. 💡 Amélioration de l'initialisation des structures table, forks, et philos.
    ✅ Améliorations globales de la lisibilité et de la gestion des erreurs.

--- src/main.c
+++ src/main.c
@@ -6,54 +6,26 @@
 /*   By: mbah <mbah@student.42lyon.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/06 00:02:15 by mbah              #+#    #+#             */
-/*   Updated: 2025/04/03 22:56:49 by mbah             ###   ########.fr       */
+/*   Updated: 2025/04/07 15:49:21 by mbah             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/philosophers.h"
-#include <pthread.h>
-
-void ft_putchar(char c)
-{
-	write(1, &c, 1);
-}
-
-static void	*func1(void *arg)
-{
-	int *n = (int *)arg;
-	(*n)++;
-	pthread_exit(NULL);
-}
-
-static void	*func2(void *arg)
-{
-	int *n = (int *)arg;
-	(*n)--;
-	pthread_exit(NULL);
-}
+#include <stdlib.h>
 
 int	main(int argc, char **argv)
 {
-	(void) argc;
-	(void) argv;
-	pthread_t	t1;
-	pthread_t	t2;
-	int			n = 100;
-	int 		i = 0;
-	struct timeval current_time;
-	pthread_mutex_t mutex;
-	
-	while (i++ < 100)
-	{
-		pthread_create(&t1, NULL, func1, &n);
-		pthread_create(&t2, NULL, func2, &n);
-		printf("n = %i\n", n);
-	}
-	pthread_join(t1, NULL);
-	pthread_join(t2, NULL);
-	pthread_mutex_init(&mutex, NULL);
- 	gettimeofday(&current_time, NULL);
-  	printf("seconds : %ld\nmicro seconds : %d",
-    	current_time.tv_sec, current_time.tv_usec);
+	t_mutex	forks[PHILO_MAX_NUMBER];
+	t_philo	philos[PHILO_MAX_NUMBER];
+	t_table	table;
+	int		nb_forks;
+
+	check_argument(argc, (const char **) argv);
+	nb_forks = ft_atoi(argv[1]);
+	init_table(&table, philos, forks);
+	init_forks(&table, forks, nb_forks);
+	init_philos(&table, philos, forks, argv);
+	run_simulation_threads(&table);
+	clean_up(&table, NULL, nb_forks, EXIT_SUCCESS);
 	return (0);
 }
