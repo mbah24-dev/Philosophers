e9a41e3ef674b038e8d365e06b692acb26940542
BAH Mamad <mbah@student.42lyon.fr>
2025-09-17 18:47:11 +0200

    philo project finished

--- philo/inc/philosophers.h
+++ philo/inc/philosophers.h
@@ -6,7 +6,7 @@
 /*   By: mbah <mbah@student.42lyon.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/06 00:02:55 by mbah              #+#    #+#             */
-/*   Updated: 2025/09/17 15:44:27 by mbah             ###   ########.fr       */
+/*   Updated: 2025/09/17 18:23:14 by mbah             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,9 +21,13 @@
 # include <limits.h>
 # include <stdbool.h>
 
-# define MAX_PHILO_NUM        250
+# ifndef DEBUG_FORMATTING
+#  define DEBUG_FORMATTING 0
+# endif
+
+# define MAX_PHILO_NUM        200
 # define MAX_INT              2147483647
-# define MAX_PHILO_STR        "250"
+# define MAX_PHILO_STR        "200"
 
 # define COLOR_RESET          "\e[0m"
 # define COLOR_RED            "\e[31m"
@@ -91,120 +95,45 @@ typedef enum e_state
 	STATE_FORK2 = 5
 }	t_state;
 
-/******************************************************
-*            INITIALIZE TABLE, PHILOS AND FORKS       *
-*******************************************************/
-
-/**
- * @brief Initializes the table with philosophers and forks.
- * @param table Pointer to the table to be initialized.
- * @param philos Pointer to the array of philosophers.
- * @param forks Pointer to the array of mutexes representing the forks.
-*/
-void	init_table(t_table *table, t_philo *philos, t_mutex *forks);
-
-/**
-  * @brief Initializes philosophers with their respective 
-  * mutexes and other properties.
-  * @param table Pointer to the table structure.
-  * @param philos Pointer to the array of philosophers.
-  * @param forks Pointer to the array of forks.
-  * @param av Array of command-line arguments.
-*/
-void	init_philos(t_table *table, t_philo *philos, t_mutex *forks, char **av);
-
-/**
-  * @brief Initializes forks by creating mutexes for them.
-  * @param table Pointer to the table structure.
-  * @param forks Pointer to the mutex array for the forks.
-  * @param quantity Number of forks (equal to the number of philosophers).
-*/
-void	init_forks(t_table *table, t_mutex *forks, int quantity);
-
-/******************************************************
-*                  EXTERNAL FUNCTIONS                 *
-*******************************************************/
-
-/**
-  * @brief Computes the length of a string.
-  * @param str The string whose length is to be computed.
-  * @return The length of the string.
-*/
-size_t	ft_strlen(const char *str);
-
-/**
-  * @brief Converts a string to an integer.
-  * @param str The string to be converted.
-  * @return The integer value represented by the string.
-*/
-int		ft_atoi(const char *str);
-
-/**
-  * @brief Gets the time elapsed since the philosopher's last meal.
-  * @param philo The philosopher whose time since last meal is to be calculated.
-  * @return The time in milliseconds since the philosopher's last meal.
-*/
-size_t	time_since_last_meal(t_philo philo);
-
 /*******************************************************
- *                  PHILO UTILS FUNCTIONS              *
+ *           PHILOSOPHERS PROJECT FUNCTIONS            *
  *******************************************************/
 
-/**
-  * @brief Displays an error message and exits the program.
-  * @param err_msg The error message to display.
-  * @param exit_status The status code for exit.
-*/
-void	an_error_occured(char *err_msg, int exit_status);
+/* cleanup file functions */
 
-/**
-  * @brief Sleeps for a specified amount of time.
-  * @param to_sleep The duration (in milliseconds) to sleep.
-*/
-void	ft_usleep(size_t to_sleep);
+void	*table_free(t_table *table);
+void	mutexes_destroy(t_table *table);
+int		msg_print(char *str, char *detail, int exit_no);
+int		error_fail(char *str, char *detail, t_table *table);
+void	*error_nullptr(char *str, char *detail, t_table *table);
 
-/**
-  * @brief Gets the current time in milliseconds.
-  * @return The current time in milliseconds.
-*/
-size_t	get_time_now(void);
+/* init file functions */
 
-/**
-  * @brief Displays an action performed by a philosopher.
-  * @param action_msg The action message to display.
-  * @param philo The philosopher who performed the action.
-*/
-void	display_action(char *action_msg, t_philo *philo);
+t_table	*table_init(int ac, char **av, int i);
 
-/**
-  * @brief Cleans up resources after the simulation and 
-  * displays an error message if necessary.
-  * @param table Pointer to the table structure.
-  * @param err_msg The error message to display.
-  * @param nb_forks Number of forks involved.
-  * @param exit_status The exit status code.
-*/
-void	clean_up(t_table *table, char *err_msg, int nb_forks, int exit_status);
+/* monitor file functions */
 
-/*******************************************************
- *              CHECK ARGUMENTS FUNCTIONS              *
- *******************************************************/
+bool	stop_check(t_table *table);
+void	*monitor(void *data);
 
-/**
-  * @brief Checks if the command-line arguments are valid.
-  * @param argc The number of arguments.
-  * @param argv Array of argument strings.
-*/
-void	check_argument(int argc, const char **argv);
+/* parsing file functions */
 
-/*******************************************************
- *             DINING SIMULATION FUNCTIONS             *
- *******************************************************/
+int		ft_atoi(char *str);
+bool	validate_arguments(int ac, char **av);
 
-/**
-  * @brief Runs the dining simulation with multiple threads.
-  * @param table Pointer to the table structure.
-*/
-void	run_simulation_threads(t_table *table);
+/* parsing file functions */
+
+void	*agent_routine(void *data);
+
+/* status file functions */
+
+void	log_state(t_agent *agent, bool reaper_report, t_state state);
+void	log_simulation_outcome(t_table *table);
+
+/* time file functions */
+
+time_t	time_now_ms(void);
+void	agent_sleep(t_table *table, time_t duration_ms);
+void	delay_start(time_t start_time);
 
 #endif
