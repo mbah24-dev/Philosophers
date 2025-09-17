882112d6fe91479a40f1c4e12ffc6743c2a03fd5
BAH Mamad <mbah@student.42lyon.fr>
2025-09-17 18:46:36 +0200

    update main file

--- philo/src/main.c
+++ philo/src/main.c
@@ -6,25 +6,94 @@
 /*   By: mbah <mbah@student.42lyon.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/06 00:02:15 by mbah              #+#    #+#             */
-/*   Updated: 2025/09/17 15:54:10 by mbah             ###   ########.fr       */
+/*   Updated: 2025/09/17 18:40:47 by mbah             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philosophers.h"
 
-int	main(int argc, char **argv)
+/**
+ * @brief Launch the simulation by creating agent threads and the reaper thread.
+ *
+ * Initializes the start time, creates a thread for each agent, and starts
+ * the grim reaper thread if there is more than one agent.
+ *
+ * @param table Pointer to the simulation table.
+ * @return true if the simulation started successfully, false on error.
+ */
+static bool	start_simulation(t_table *table)
 {
-	t_mutex	forks[PHILO_MAX_NUMBER];
-	t_philo	philos[PHILO_MAX_NUMBER];
-	t_table	table;
-	int		nb_forks;
+	unsigned int	i;
 
-	check_argument(argc, (const char **) argv);
-	nb_forks = ft_atoi(argv[1]);
-	init_table(&table, philos, forks);
-	init_forks(&table, forks, nb_forks);
-	init_philos(&table, philos, forks, argv);
-	run_simulation_threads(&table);
-	clean_up(&table, NULL, nb_forks, EXIT_SUCCESS);
-	return (0);
+	table->start_time = time_now_ms() + (table->nb_agents * 2 * 10);
+	i = 0;
+	while (i < table->nb_agents)
+	{
+		if (pthread_create(&table->agents[i]->thread, NULL,
+				&agent_routine, table->agents[i]) != 0)
+			return (error_fail(MSG_ERR_THREAD, NULL, table));
+		i++;
+	}
+	if (table->nb_agents > 1)
+	{
+		if (pthread_create(&table->reaper, NULL,
+				&monitor, table) != 0)
+			return (error_fail(MSG_ERR_THREAD, NULL, table));
+	}
+	return (true);
+}
+
+/**
+ * @brief Stop the simulation by joining threads and cleaning resources.
+ *
+ * Joins all agent threads and the reaper thread, prints outcome 
+ * if debug is enabled,
+ * destroys mutexes, and frees allocated memory.
+ *
+ * @param table Pointer to the simulation table.
+ */
+static void	stop_simulation(t_table *table)
+{
+	unsigned int	i;
+
+	i = 0;
+	while (i < table->nb_agents)
+	{
+		pthread_join(table->agents[i]->thread, NULL);
+		i++;
+	}
+	if (table->nb_agents > 1)
+		pthread_join(table->reaper, NULL);
+	if (DEBUG_FORMATTING == true && table->meals_required != -1)
+		log_simulation_outcome(table);
+	mutexes_destroy(table);
+	table_free(table);
+}
+
+/**
+ * @brief Entry point of the program.
+ *
+ * Validates input arguments, initializes the simulation table, starts
+ * the simulation, and stops it when finished.
+ *
+ * @param ac Argument count.
+ * @param av Argument vector.
+ * @return Exit status.
+ */
+int	main(int ac, char **av)
+{
+	t_table	*table;
+
+	table = NULL;
+	if (ac - 1 < 4 || ac - 1 > 5)
+		return (msg_print(MSG_USAGE, NULL, EXIT_FAILURE));
+	if (!validate_arguments(ac, av))
+		return (EXIT_FAILURE);
+	table = table_init(ac, av, 1);
+	if (!table)
+		return (EXIT_FAILURE);
+	if (!start_simulation(table))
+		return (EXIT_FAILURE);
+	stop_simulation(table);
+	return (EXIT_SUCCESS);
 }
